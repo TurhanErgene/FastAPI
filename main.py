73e401040563ba0360f7c21e66c0097e48f3b8c7
Turhan <turhan-ergene@hotmail.com>
2022-05-25 22:39:57 +0200

    Optional and def index

--- main.py
+++ main.py
@@ -1,24 +1,34 @@
-from fastapi import FastAPI    # http://127.0.0.1:8000/docs # http://127.0.0.1:8000/redoc
+# http://127.0.0.1:8000/docs # http://127.0.0.1:8000/redoc
+from typing import Optional
+from fastapi import FastAPI
 
-app = FastAPI() #uvicorn main:app --reload
-
-
-@app.get('/') ##decorator
-def index():
-  return {'data': 'home page'}
+app = FastAPI()  # uvicorn main:app --reload
 
 
-@app.get('/blog/unpublished') ## to avoid conflicts between routes; this need to be above /blog/{id}
+# decorator /blog?limit=10&published=true
+# (limit: int = 10, published: bool = True)
+# only get 10 published blog posts
+
+@app.get('/blog') ##using Optional helps not to recieve error if params are not provided
+def index(limit: int = 10, published: bool = True, sort: Optional[str] = None):
+    if published:
+        return {'data': f'{limit} published blogs from the db'}
+    else:
+        return {'data': f'{limit} blogs from the db'}
+
+
+# to avoid conflicts between routes; this need to be above /blog/{id}
+@app.get('/blog/unpublished')
 def unpublished():
-  return {'data': 'all unpublished blogs'}
+    return {'data': 'all unpublished blogs'}
 
 
 @app.get('/blog/{id}')
 def show(id: int):
-  # fetch blog with id = id
-  return {'data': id}
+    # fetch blog with id = id
+    return {'data': id}
 
 
 @app.get('/blog/{id}/comments')
-def comments():
-  return {'data': {'comments': 'comment 1'}}
+def comments(id, limit = 10):
+    return {'data': {'comments': 'comment 1'}}
