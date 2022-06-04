6b6f356db7efbe90d7e6540e2fc9121cb879d5b5
Turhan <turhan-ergene@hotmail.com>
2022-06-04 03:20:33 +0200

    created models and tables, store blog to database

--- blog/main.py
+++ blog/main.py
@@ -1,10 +1,27 @@
-from fastapi import FastAPI
-from . import schemas
+from fastapi import FastAPI, Depends
+from . import schemas, models
+from .database import engine, SessionLocal
+from sqlalchemy.orm import Session
+
+
 app = FastAPI()
 
 
+models.Base.metadata.create_all(engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db ##yield is a keyword that is used like return, except the function will return a generator.
+    finally:
+        db.close()
+
 
 @app.post('/blog')
-def create(request: schemas.Blog):
-    return request
+def create(request: schemas.Blog, db : Session = Depends(get_db)):
+    new_blog = models.Blog(title = request.title, body = request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
 
