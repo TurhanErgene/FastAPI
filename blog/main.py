42cfc23f05f4c2176b72e52381a616c91c02336d
Turhan <turhan-ergene@hotmail.com>
2022-06-07 16:50:59 +0200

    http exceptions

--- blog/main.py
+++ blog/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Response, status, HTTPException
 from . import schemas, models
 from .database import engine, SessionLocal
 from sqlalchemy.orm import Session
@@ -9,30 +9,37 @@ app = FastAPI()
 
 models.Base.metadata.create_all(engine)
 
+
 def get_db():
     db = SessionLocal()
     try:
-        yield db ##yield is a keyword that is used like return, except the function will return a generator.
+        yield db  ##yield is a keyword that is used like return, except the function will return a generator.
     finally:
         db.close()
 
 
-@app.post('/blog')
-def create(request: schemas.Blog, db : Session = Depends(get_db)):
-    new_blog = models.Blog(title = request.title, body = request.body)
+@app.post("/blog", status_code=status.HTTP_201_CREATED)
+def create(request: schemas.Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=request.title, body=request.body)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
     return new_blog
 
 
-@app.get('/blog/')
-def all(db : Session = Depends(get_db)):
+@app.get("/blog/")
+def all(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
 
-@app.get('/blog/{id}')
-def one(id: int, db : Session = Depends(get_db)):
+@app.get("/blog/{id}", status_code=200)
+def one(id: int, response: Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(
+            status_code=404, detail=f"Blog with the id: {id} has not found"
+        )
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {"detail": f"Blog with the id: {id} has not found"}
     return blog
